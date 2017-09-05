56a044ddf4c1d8764370cba64b633c9847e74319
Aaron Turner <aaron@aaronthedev.com>
2017-09-05 12:21:00 -0700

    Got a working handlebars example, added links to libs in readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # piStreamRadio-frontend
-A frontend web client for piStreamRadio. Using Gin, AMP, and MustacheJS
+A frontend web client for piStreamRadio. Using [Gin](https://github.com/gin-gonic/gin), [AMP](https://github.com/ampproject/amphtml), and [MustacheJS with raymond](https://github.com/aymerick/raymond).
 
 Start with:
 
--- main.go
+++ main.go
@@ -1,10 +1,57 @@
 package main
 
+import "fmt"
 import "github.com/gin-gonic/gin"
-import "github.com/Jeffail/gabs"
+import "encoding/json"
+import "github.com/aymerick/raymond"
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		// Some sample json, TODO: need to read froma  file
+		sampleJson := []byte(`{
+			"title": "test",
+			"body": "ayyeee lmao",
+			"deep": {
+				"rolling": "in the deep"
+			}
+		}`)
+
+		// Unmarshal the json
+		// https://gobyexample.com/json
+		var jsonMarshal map[string]interface{}
+		err := json.Unmarshal(sampleJson, &jsonMarshal);
+		if err != nil {
+			panic(err)
+		}
+
+		// Our example template need to TODO: read from a file
+		tpl := `<div class="entry">
+		hello testing!
+		 <h1>{{title}}</h1>
+		 <div class="body">
+			 {{body}}
+			 <div>
+			 	{{deep}}
+			 </div>
+			 <div>
+			 	{{deep.rolling}}
+			 </div>
+		 </div>
+		</div>
+		`
+
+		// Render our handlebars Template
+		result, err := raymond.Render(tpl, jsonMarshal)
+    if err != nil {
+        panic("Please report a bug :)")
+    }
+
+		fmt.Println("Rendered /")
+
+		// Return our html
+		c.Data(200, "text/html; charset=utf-8", []byte(result))
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
