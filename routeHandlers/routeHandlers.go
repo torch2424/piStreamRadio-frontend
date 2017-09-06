f21f43ee7240d94cd2bf9a212eb30d8eae36312a
Aaron Turner <aaron@aaronthedev.com>
2017-09-06 09:31:18 -0700

    Finished the playlist, and started finalizing public pages

--- routeHandlers/routeHandlers.go
+++ routeHandlers/routeHandlers.go
@@ -2,7 +2,6 @@ package routeHandlers
 
 // Imports
 import (
-  "fmt"
   "path/filepath"
   "os"
 )
@@ -23,18 +22,48 @@ func Home(c *gin.Context) {
   jsonParsed, _ := gabs.ParseJSON(renderer.ReadFileAsByte("./templates/pages/home/home.json"))
   jsonParsed.SetP("I changed the body again with gabssss! lmaooo ayyeee", "body");
 
-  fmt.Println("Rendering / ...")
-
   // Return our html
   c.Data(200, "text/html; charset=utf-8",
     []byte(renderer.RenderFromRawJSON("./templates/pages/home/home.html",
       []byte(jsonParsed.String()))))
 }
 
+// About Handler for the "/about" Route. Simply renders HTML and JSON
+func About(c *gin.Context) {
+  // Return our html
+  c.Data(200, "text/html; charset=utf-8",
+    []byte(renderer.RenderFromPath("./templates/pages/about/about.html", "./templates/pages/about/about.json")))
+}
+
+// Contact Handler for the "/contact" Route. Simply renders HTML and JSON
+func Contact(c *gin.Context) {
+  // Return our html
+  c.Data(200, "text/html; charset=utf-8",
+    []byte(renderer.RenderFromPath("./templates/pages/contact/contact.html", "./templates/pages/contact/contact.json")))
+}
+
 // Playlist the handler for the "/playlist" route. Will Show Songs on the playlist
 func Playlist(c *gin.Context) {
-  configJSON, _ := gabs.ParseJSON(renderer.ReadFileAsByte("./config.json"))
-  musicDir, _ := configJSON.Path("musicFilesPath").Data().(string)
+  // Read our config JSON for the music directory
+  configJSON, configJSONErr := gabs.ParseJSON(renderer.ReadFileAsByte("./config.json"))
+  if configJSONErr != nil {
+    panic(configJSONErr)
+  }
+
+  // Get our Music Directory from out config JSON
+  musicDir, doesExist := configJSON.Path("musicFilesPath").Data().(string)
+  if doesExist == false {
+    panic("config.json: musicFilesPath Does Not Exist in JSON")
+  }
+
+  // Get our playlist JSON, and set an array on songs
+  playlistJSON, playlistJSONErr := gabs.ParseJSON(renderer.ReadFileAsByte("./templates/pages/playlist/playlist.json"))
+  if playlistJSONErr != nil {
+    panic(playlistJSONErr)
+  }
+  playlistJSON.ArrayP("songs");
+
+  // Iterate through the directory (Already in Lexical order, may need to sort the song JSON by Artist, and then song)
   filepath.Walk(musicDir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
       // Open the file for reading
@@ -44,17 +73,27 @@ func Playlist(c *gin.Context) {
       }
 
       // Send the file to the meta data tagger
-      m, err := tag.ReadFrom(file)
+      metaData, err := tag.ReadFrom(file)
       if err != nil {
       	panic(err)
       }
 
-      // TODO: Add the info to the playlist JSON
-      fmt.Println(m.Title())
+      songTitle := metaData.Title()
+      songArtist := metaData.Artist()
+
+      if len(songTitle) > 0 && len(songTitle) > 0 {
+        // Create the Song JSON
+        songJSON := gabs.New()
+        songJSON.SetP(songArtist, "artist")
+        songJSON.SetP(songTitle, "title")
+
+        // Add the song JSON to the playlist songs array
+        playlistJSON.ArrayAppend(songJSON.Data(), "songs")
+      }
 		}
 		return nil
 	})
 
 c.Data(200, "text/html; charset=utf-8",
-  []byte(renderer.RenderFromPath("./templates/pages/playlist/playlist.html", "./templates/pages/playlist/playlist.json")))
+  []byte(renderer.RenderFromRawJSON("./templates/pages/playlist/playlist.html", []byte(playlistJSON.String()))))
 }
